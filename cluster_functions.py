9e9c2593104ef3323defe2c5cbb564e0b3364492
Leonardo Tadeu <77113853+leonardotadeufss@users.noreply.github.com>
2023-07-02 16:36:21 -0300

    Update cluster_functions.py

--- cluster_functions.py
+++ cluster_functions.py
@@ -76,15 +76,39 @@ def create_user(host_name, username):
     except subprocess.CalledProcessError as e:
         print("An error occurred during remote user creation:", str(e))
 
+def help():
+    commands = {
+        '[host_name] copy_file': ('Copy a file locally from source path to destination path and do the same on a remote machine.',
+                      '[source_path]', '[destination_path]'),
+        '[host_name] move_file': ('Move a file locally  from source path to destination path and do the same on a remote machine.',
+                      '[source_path]', '[destination_path]'),
+        '[host_name] change_file_permissions': ('Change the permissions of a file on a local and remote machine.',
+                                    '[file_name]', '[permissions_list]'),
+        '[host_name] create_user': ('Create a new user locally and on a remote machine.',
+                        '[username]'),
+        '[host_name] ls': ('List files and directories in a given path.','[filters]')
+    }
 
+    print()
+    print("Available commands:")
+    for command, info in commands.items():
+        description, *parameters = info
+        parameters_string = ' '.join(parameters)
+        print(f"{command} {parameters_string}: {description}")
+    print()
 def main():
     command = sys.argv[1:]
+    if len(command) == 0 or command[0] == "help" :
+        help()
+        return
     host_name = command[0]
     function_name = command[1]
     args = command[2:]
 
+
     if function_name == "ls":
         ls_clusters(host_name, *args)
+
     elif function_name == "copy_file":
         source_path = args[0]
         destination_path = args[1]
